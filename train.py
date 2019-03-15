dfb6f5be139c5339a3f9dc5c4770ce75821f8244
Evgeniya <vremendm@gmail.com>
2019-03-15 18:29:20 +0100

    add optimizer to config

--- train.py
+++ train.py
@@ -2,10 +2,13 @@ import argparse
 import json
 import os
 from typing import List
+from torch.optim.adam import Adam
+from torch.optim.sgd import SGD
 
 from flair.data_fetcher import NLPTaskDataFetcher, NLPTask
 from flair.data import TaggedCorpus
-from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings, CharLMEmbeddings, CharacterEmbeddings
+from flair.embeddings import WordEmbeddings, StackedEmbeddings, PooledFlairEmbeddings, CharacterEmbeddings, \
+    BertEmbeddings
 from flair.training_utils import EvaluationMetric
 from flair.visual.training_curves import Plotter
 from flair.trainers import ModelTrainer
@@ -15,7 +18,7 @@ from flair.models import SequenceTagger
 def train(params):
     # 1. get the corpus
     print(NLPTask[params["task"]])
-    corpus = NLPTaskDataFetcher.load_corpus(task = NLPTask[params["task"]], files = params['filenames'])
+    corpus = NLPTaskDataFetcher.load_corpus(task=NLPTask[params["task"]], files=params['filenames'])
     print(corpus)
 
     # 2. what tag do we want to predict?
@@ -30,13 +33,16 @@ def train(params):
     if params["word_embeddings"] is True:
         embedding_types.append(WordEmbeddings(params["embeddings_name"]))
 
+    if params["bert_embeddings"] is True:
+        embedding_types.append(BertEmbeddings(params["bert_model"]))
+
     if params["char_embeddings"] is True:
         # comment in this line to use character embeddings
         embedding_types.append(CharacterEmbeddings())
 
     if params["charlm_embeddings"] is True:
-        embedding_types.append(CharLMEmbeddings('news-forward'))
-        embedding_types.append(CharLMEmbeddings('news-backward'))
+        embedding_types.append(PooledFlairEmbeddings('news-forward', pooling='min'))
+        embedding_types.append(PooledFlairEmbeddings('news-backward', pooling='min'))
 
     embeddings = StackedEmbeddings(embeddings=embedding_types)
 
@@ -48,7 +54,8 @@ def train(params):
                             tag_type=tag_type,
                             use_rnn=params["use_rnn"],
                             rnn_layers=params["rnn_layers"],
-                            use_crf=params["use_crf"])
+                            use_crf=params["use_crf"]
+                            )
 
     base_path = os.path.join(params["model_dir"], params["model_tag"])
     os.makedirs(base_path, exist_ok=True)
@@ -57,7 +64,14 @@ def train(params):
         json.dump(params, cfg)
     # initialize trainer
 
-    trainer: ModelTrainer = ModelTrainer(tagger, corpus)
+    if "optimizer" in params:
+        if params["optimizer"] == "adam":
+            optim = Adam
+        elif params["optimizer"] == "sgd":
+            optim = SGD
+    else:
+        optim = SGD
+    trainer: ModelTrainer = ModelTrainer(tagger, corpus, optimizer=optim)
 
     trainer.train(base_path, EvaluationMetric.MICRO_F1_SCORE, mini_batch_size=params["mini_batch_size"],
                   max_epochs=params["max_epochs"], save_final_model=params["save_model"],
